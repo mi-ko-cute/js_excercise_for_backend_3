81670cd71f0b1f69b45d19cd1575c237d1c9b920
mi-ko-cute <thx-mail@worysolve.com>
2019-11-03 14:31:39 +0900

    指摘を受けて修正

--- test/src/Counter.spec.js
+++ test/src/Counter.spec.js
@@ -4,36 +4,36 @@ const Counter = require('../../src/Counter.js');
 describe('Counterクラスのテスト', () => {
     it('countプロパティを持つことを確認する', () => {
         const counter = new Counter();
-        assert.ok(counter.hasOwnProperty._count);
-    })
+        assert.ok(counter.hasOwnProperty(_count));
+    });
 
     it('_counterプロパティの初期値は0', () => {
         const counter = new Counter();
-        assert.equal(counter._count, 0);
-    })
+        assert.strictEqual(counter._count, 0);
+    });
 
     it('incrementメソッドを持つ', () => {
         const counter = new Counter();
-        assert.ok(typeof counter.increment() === 'function');
-    })
+        assert.ok(typeof counter.increment === 'function');
+    });
 
     it('incrementメソッドを実行すると_countプロパティの値は1増える', () => {
         const counter = new Counter();
 
         counter.increment();
-        assert.equal(counter._count, 1);
-    })
+        assert.strictEqual(counter._count, 1);
+    });
 
     it('decrementメソッドを持つ', () => {
         const counter = new Counter();
-        assert.ok(typeof counter.decrement() === 'function');
-    })
+        assert.ok(typeof counter.decrement === 'function');
+    });
 
     it('decrementメソッドを実行すると_countプロパティの値は1減る', () => {
         const counter = new Counter();
 
         counter.decrement();
-        assert.equal(counter._count, -1);
-    })
+        assert.strictEqual(counter._count, -1);
+    });
 
-})
+});
